a48649cd6b9a69df67ed2d38d8636a49024896a3
Ann Blodgett <w.annblodgett@gmail.com>
2016-04-16 11:19:29 -0600

    Shadow Guard
    Shadow Guard
    Level Goals: 1) Avoid the spikes. 2) Collect the Gems. 3) Stay out of sight of the ogre.
    Necessary Equipment - SIMPLE BOOTS
    Concepts practiced - basic syntax

---
+++ shadow_guard.py
@@ -0,0 +1,11 @@
+#Shadow Guard
+#Level Goals: 1) Avoid the spikes. 2) Collect the Gems. 3) Stay out of sight of the ogre.
+# Necessary Equipment - SIMPLE BOOTS
+
+#!!- Begin Code-!!
+# Stay out of sight of the ogre. Grab the gems.
+self.moveRight()
+self.moveUp()
+self.moveRight()
+self.moveDown()
+self.moveRight()
