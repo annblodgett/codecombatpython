33764c7dcfd0209243d9cad81aba9e41b4dbe772
Ann Blodgett <w.annblodgett@gmail.com>
2016-04-16 11:10:32 -0600

    Dungeons of Kithgard
     Dungeons of Kithgard 
    Level Goals: 1) Avoid the spikes. 2) Collect the Gem.
    Necessary Equipment - SIMPLE BOOTS

---
+++ dungeons_of_kithgard.py
@@ -0,0 +1,13 @@
+# Dungeons of Kithgard 
+#Level Goals: 1) Avoid the spikes. 2) Collect the Gem.
+# Necessary Equipment - SIMPLE BOOTS
+
+#!!- Begin Code-!!
+# Move to the gem.
+# Don't touch the walls!
+# Type your code below.
+
+self.moveRight()
+self.moveDown()
+self.moveRight()
+self.moveRight()
