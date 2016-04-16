c17f03c4c3b4b432aeafc46077ee296453146c49
Ann Blodgett <w.annblodgett@gmail.com>
2016-04-16 11:28:58 -0600

    Signs and Portents - initial commit
    Signs and Portents - Escape the area while your allies hold off waves of ogres!
    Concepts Learned - Basic Syntax
    Level Goals: 1) Find Senick and Omarn 2) Protect them with the lightstone
    Bonus Goals: None
    Equipment Needed: SIMPLE BOOTS

---
+++ signs_and_portents.py
@@ -0,0 +1,16 @@
+#Signs and Portents - Escape the area while your allies hold off waves of ogres!
+#Concepts Learned - Basic Syntax
+#Level Goals: 1) Find Senick and Omarn 2) Protect them with the lightstone
+#Bonus Goals: None
+#Equipment Needed: SIMPLE BOOTS
+
+#!!- Begin Code-!!
+# Your goal is to get to the right side of the map alive.
+# You don't need to fight the ogres, just move! Your allies will protect you.
+self.moveRight(2)
+self.moveUp(1)
+self.moveRight(3)
+self.moveDown(1)
+self.moveRight(1)
+self.moveDown(1)
+self.moveRight(1)
