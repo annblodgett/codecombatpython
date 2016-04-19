e900bd33fe6ca2cbbb73443fc6a0dea2f58335e8
Ann Blodgett <w.annblodgett@gmail.com>
2016-04-19 16:42:41 -0600

    Initial upload the_raised_sword and enemy_mine
    #The Raised Sword - Learn to equip yourself for combat.
    #Concepts Learned - Basic Syntax, Strings
    #Level Goals: 1) Defeat the ogres. 2) Your hero must survive. 
    #Bonus Goals: no code problems
    #Necessary Equipment:SHARPENED SWORD
    
    
    Enemy Mine - Tread carefully, danger is afoot!
    Concepts Learned - Basic Syntax, Arguments
    Level Goals: 1) Avoid the fire traps! 2) Lure the ogre to its doom  3)Get the Gem
    Necessary Equipment:SIMPLE BOOTS

---
+++ enemy_mine.py
@@ -0,0 +1,16 @@
+#Enemy Mine - Tread carefully, danger is afoot!
+#Concepts Learned - Basic Syntax, Arguments
+#Level Goals: 1) Avoid the fire traps! 2) Lure the ogre to its doom  3)Get the Gem
+#Necessary Equipment:SIMPLE BOOTS
+
+
+#!!- Begin Code-!!
+# Use arguments with move statements to move farther.
+self.moveRight(3)
+self.moveUp(1)
+self.moveRight(1)
+self.moveDown(3)
+self.moveRight(2)
+
+   
+
---
+++ the_raised_sword.py
@@ -0,0 +1,17 @@
+#The Raised Sword - 
+#Concepts Learned - Basic Syntax, Strings
+#Level Goals: 1) Defeat the ogres. 2) Your hero must survive. 
+#Bonus Goals: no code problems
+#Necessary Equipment:SHARPENED SWORD
+
+
+#!!- Begin Code-!!
+# Defeat the ogres.
+# Remember that they each take two hits.
+while True:
+    enemy = self.findNearestEnemy()
+    self.attack(enemy)
+    self.attack(enemy)
+
+   
+
