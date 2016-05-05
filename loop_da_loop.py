97e404bb94f1f691d44a65ee28e884d25a09ff56
Ann Blodgett <w.annblodgett@gmail.com>
2016-05-04 19:16:32 -0600

    REALLY added loop da loop
     - forgot to add the file last time! 
    
    #Loop Da Loop - Loops are a life saver!
    #Concepts Learned - Basic Syntax, While Loops
    #Level Goals: 1) Avoid the deadly lightning beam 2) Under 6 Statements
    #Bonus Goals: None
    #Necessary Equipment: SIMPLE BOOTS

---
+++ loop_da_loop.py
@@ -0,0 +1,18 @@
+#Loop Da Loop - Loops are a life saver!
+#Concepts Learned - Basic Syntax, While Loops
+#Level Goals: 1) Avoid the deadly lightning beam 2) Under 6 Statements
+#Bonus Goals: None
+#Necessary Equipment: SIMPLE BOOTS
+
+#!!- Begin Code-!!
+# The code in this loop will repeat forever.
+
+while True:
+    # Move right
+    self.moveRight()
+    # Move up
+    self.moveUp()
+    # Move left
+    self.moveLeft()
+    # Move down
+    self.moveDown()
