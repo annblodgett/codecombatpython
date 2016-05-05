c221c82173c37d6ac848f10326c3a8c1d0ee439d
Ann Blodgett <w.annblodgett@gmail.com>
2016-05-04 19:11:37 -0600

    Add fire_dancing and haunted_kithmaze
    #Fire Dancing - Save typing (and your hero) with loops!
    #Concepts Learned - Basic Syntax, While Loops
    #Level Goals: 1) Dodge the fireballs forever 2) Under 4 Statements
    #Bonus Goals: None
    #Necessary Equipment: SIMPLE BOOTS
    -------------------------------------------------------------------------------------------
    #Haunted Kithmaze - A maze constructed to confuse travelers
    #Concepts Learned - Basic Syntax, While Loops
    #Level Goals: 1) Your hero must survive. 2) Navigate the maze 3) Under 6 Statements
    #Bonus Goals: None
    #Necessary Equipment: SIMPLE BOOTS

---
+++ fire_dancing.py
@@ -0,0 +1,14 @@
+#Fire Dancing - Save typing (and your hero) with loops!
+#Concepts Learned - Basic Syntax, While Loops
+#Level Goals: 1) Dodge the fireballs forever 2) Under 4 Statements
+#Bonus Goals: None
+#Necessary Equipment: SIMPLE BOOTS
+
+#!!- Begin Code-!!
+# Code normally executes in the order it's written.
+# Loops repeat a block of code multiple times.
+# Use tab or 4 spaces to indent the move lines under the loop.
+
+while True:
+    self.moveRight()
+    self.moveLeft()# Add a moveLeft command to the loop here
---
+++ haunted_kithmaze.py
@@ -0,0 +1,14 @@
+#Haunted Kithmaze - A maze constructed to confuse travelers
+#Concepts Learned - Basic Syntax, While Loops
+#Level Goals: 1) Your hero must survive. 2) Navigate the maze 3) Under 6 Statements
+#Bonus Goals: None
+#Necessary Equipment: SIMPLE BOOTS
+
+#!!- Begin Code-!!
+# Loops are a better way of doing repetitive things.
+
+while True:
+    # Add commands in here to repeat.
+    self.moveRight(2)
+    self.moveUp(2)
+	
